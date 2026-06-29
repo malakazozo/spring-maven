cead0d5c01ce9d6bdabde5f1b7b09ac82c2c5f11
malakazozo <malak.azeba@efrei.net>
2026-06-29 13:18:12 +0200

    Fix CI: make mvnw executable in CI

--- src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -1,24 +1,25 @@
-icipackage com.example.demo_github;
-
+package com.example.demo_github;
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
+ 
 @SpringBootApplication
 public class DemoGithubApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoGithubApplication.class, args);
-	}
-
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubApplication.class, args);
+    }
+ 
 }
-
+ 
 @RestController
 class HelloWorldController {
+ 
     @GetMapping("/")
     public String sayHello() {
         return "Hello, World!";
     }
+ 
 }
