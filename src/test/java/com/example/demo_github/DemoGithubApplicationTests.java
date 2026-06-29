d981bfa482b5c36fe6c052539575d6ca16235f25
malakazozo <malak.azeba@efrei.net>
2026-06-29 13:23:39 +0200

    Update DemoGithubApplicationTests.java

--- src/test/java/com/example/demo_github/DemoGithubApplicationTests.java
+++ src/test/java/com/example/demo_github/DemoGithubApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo_github;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoGithubApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@SpringBootApplication
+public class DemoGithubApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubApplication.class, args);
+    }
+ 
+}
+ 
+@RestController
+class HelloWorldController {
+ 
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+ 
 }
